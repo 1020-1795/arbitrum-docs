25f21de2c1c2037925a573eab19d85454670b42f
GitHub Action <action@github.com>
2026-08-31 15:33:47 +0000

    chore: update dependencies and Nitro Docker image
    Updated projects:
    Arbitrum Nitro, Arbitrum Stylus SDK, Arbitrum Orbit SDK, Arbitrum Nitro Contracts

--- src/resources/globalVars.js
+++ src/resources/globalVars.js
@@ -36,7 +36,7 @@ const globalVars = {
   l1AvgBlockTimeSeconds: L1_BLOCK_TIME_SECONDS,
 
   // Node docker images
-  latestNitroNodeImage: 'offchainlabs/nitro-node:v3.11.3-beb2108',
+  latestNitroNodeImage: 'offchainlabs/nitro-node:v3.11.3-4130f4c',
   latestClassicNodeImage: 'offchainlabs/arb-node:v1.4.6-551a39b3',
 
   // Node snapshots (taken around April 20th, 2013)
